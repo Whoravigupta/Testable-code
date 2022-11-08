a762ad5f43868b6b67f7a5d94f26afef67c6f55c
Whoravigupta <117490874+Whoravigupta@users.noreply.github.com>
2022-11-07 22:58:14 -0600

    unit test

---
+++ Testable code/Unit test/Class1.cs
@@ -0,0 +1,19 @@
+﻿
+if (defined('UNIT_TESTING') && UNIT_TESTING)
+{
+    function my_rand($min, $max)
+    {
+        return $GLOBALS['random_table'][$min][$max];
+    }
+}
+else
+{
+    function my_rand($min = 0, $max = PHP_INT_MAX)
+    {
+        if ($max === PHP_INT_MAX)
+      {
+         $max = getrandmax();
+    }
+    return rand($min, $max);
+}
+}

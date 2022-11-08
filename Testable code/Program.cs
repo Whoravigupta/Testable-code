e84d6e25ba7083190d320c9c5a2f3ae0f5f3bb3f
Whoravigupta <117490874+Whoravigupta@users.noreply.github.com>
2022-11-07 22:38:59 -0600

    Add project files.

---
+++ Testable code/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        Random r = new Random();
+        int genRand = r.Next(10, 50);
+        Console.WriteLine("Random Number = " + genRand);
+    }
+} 
